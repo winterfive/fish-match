97e05f5de7e9e252ce8545f78dd4ac93f1489e5f
Lee Gainer <piealacode@gmail.com>
2019-07-31 18:01:14 -0400

    Initial files set up

---
+++ src/components/Card.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card"/>
+</template>
+
+<script>
+export default {
+  name: "Card"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.card {
+    background-color: blue;
+    height: 100px;
+    width: 50px;
+    border-radius: 10px;
+}
+</style>
---
+++ src/components/Game.vue
@@ -0,0 +1,29 @@
+<script>
+import Card from "./Card";
+
+export default {
+  name: "Game",
+  components: {
+    Card
+  }   
+}
+</script>
+
+<template>
+  <v-container>
+    <v-select>
+      <header>Game Name</header>
+    </v-select>
+    <v-select row wrap>
+      <v-flex>
+        <v-card v-for="c in 10" :key="c"><Card/></v-card>
+      </v-flex>
+    </v-select>
+  </v-container>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+
+
