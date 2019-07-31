50a2eb5f1e920088d578890ddc4e893248e4b08b
Lee Gainer <piealacode@gmail.com>
2019-07-31 18:58:07 -0400

    Added template code, removed default file

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,29 +1,90 @@
-<script>
-import Card from "./Card";
-
-export default {
-  name: "Game",
-  components: {
-    Card
-  }   
-}
-</script>
-
 <template>
-  <v-container>
-    <v-select>
-      <header>Game Name</header>
-    </v-select>
-    <v-select row wrap>
-      <v-flex>
-        <v-card v-for="c in 10" :key="c"><Card/></v-card>
-      </v-flex>
-    </v-select>
-  </v-container>
+  <v-app>    
+    <v-app-bar
+      app
+      color="indigo darken-3"
+      dark
+    >
+      <v-toolbar-title>Game Name</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-app-bar-nav-icon @click="openOptions"></v-app-bar-nav-icon>
+    </v-app-bar>
+    <v-content>
+      <v-container
+        fluid
+        fill-height
+      >
+        <v-layout
+          justify-center
+          align-center
+        >
+          <v-flex shrink>
+            <v-tooltip right>
+              <template v-slot:activator="{ on }">
+                <v-btn
+                  :href="source"
+                  icon
+                  large
+                  target="_blank"
+                  v-on="on"
+                >
+                  <v-icon large>mdi-code-tags</v-icon>
+                </v-btn>
+              </template>
+              <span>Source</span>
+            </v-tooltip>
+            <v-tooltip right>
+              <template v-slot:activator="{ on }">
+                <v-btn
+                  icon
+                  large
+                  href="https://codepen.io/johnjleider/pen/QewYYx"
+                  target="_blank"
+                  v-on="on"
+                >
+                  <v-icon large>mdi-codepen</v-icon>
+                </v-btn>
+              </template>
+              <span>Codepen</span>
+            </v-tooltip>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+
+    <v-navigation-drawer
+      v-model="right"
+      fixed
+      right
+      temporary
+    ></v-navigation-drawer>
+
+    <v-footer
+      app
+      color="indigo darken-3"
+      class="white--text"
+    >
+      <span>Made with PIE</span>
+      <v-spacer></v-spacer>
+      <span>&copy; 2019</span>
+    </v-footer>
+  </v-app>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
-
+<script>
+  export default {
+    props: {
+      source: String,
+    },
+    data: () => ({
+      drawer: null,
+      right: false
+    }),
+    methods: {
+      openOptions() {
+        console.log("options clicked");
+      }
+    }
+  }
+</script>
 
